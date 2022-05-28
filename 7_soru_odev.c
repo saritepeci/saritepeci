a140050e50374f0222d4105cc8c7a81f01dad994
saritepeci <105279573+saritepeci@users.noreply.github.com>
2022-05-29 00:14:19 +0200

    Create 7_soru_odev.c

---
+++ 7_soru_odev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+
+{
+switch(printf("Hi-"))
+{
+
+case 1: printf("A"); break;
+case 2: printf("B"); break;
+case 3: printf("C"); break;
+
+default:  printf("default");
+
+}
+
+return 0;
+}
